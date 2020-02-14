9ae5a56f0c0ca15a1865fcadd9eaeda3fbaf6a9c
fareselkhouli <fareselkhouli@gmail.com>
2020-02-14 15:52:27 -0500

    Update Date.java
    test
    
    Co-Authored-By: Elaineanif <elaineanif@users.noreply.github.com>

--- src/Date.java
+++ src/Date.java
@@ -1,39 +1,42 @@
+import java.util.StringTokenizer;
+
 /**
   This class reresents the data Object
  @Fares Elkhouli @Zhiyu Feng
  */
-public class Date 
+public class Date
 {
    private int  day;
    private int  month;
    private int  year;
-   
+
+   //creates date object by parsing input string
    public Date(String d)
    {
-      //use StringTokenizer to parse the String and create a Date object     
+      //use StringTokenizer to parse the String and create a Date object
+       StringTokenizer date = new StringTokenizer("d");
+
    }
-   
+
    public Date(Date d)
    {
       //this is a constructor
-   }      
-   
+   }
+
    public boolean isValid()
    {
-       
+
    }
-   
+
    @Override
    public String toString()
    {
        //use the format "month/day/year"
    }
-   
+
    @Override
    public boolean equals(Object obj)
    {
-       
-   }  
+
+   }
 }
-
-
