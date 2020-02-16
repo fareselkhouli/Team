ea3e1a9fef5c692152aff809d670a63512ed1fb8
fareselkhouli <fareselkhouli@gmail.com>
2020-02-16 15:03:37 -0500

    Update TeamMember.java
    Finished TeamMember class. Testing still to do.

--- src/TeamMember.java
+++ src/TeamMember.java
@@ -1,30 +1,53 @@
 /**
- 
- @author  
+ This class represents the team member object
+ @FaresElkhouli
+ @ZhiyuFeng
  */
 public class TeamMember 
 {
    private String name;
    private Date   startDate;
-   
+
+   /**
+    * TeamMember constructor
+    * @param String name of team member
+    * @param Date start date of team member
+    */
    public TeamMember(String nm, Date date)
    {
-       
+       this.name = nm;
+       this.startDate = date;
    }
-   
+
+   /**
+    * Returns the start date of a team member
+    * @return Date startdate of teammember
+    */
    public Date getStartDate()
    {
-      
+      return new Date(startDate);
    }
-   
+
+   /**
+    * checks if two teammembers are equal
+    * @param teammember object
+    * @return true if name and startdate are the same, false if they are not
+    */
    public boolean equals(Object obj)
    {
       //name and startDate must be the same
-   }  
-   
+      return (obj.name.equals(this.name) && obj.getStartDate.equals(starDate));
+   }
+
+   /**
+    * String representation of a teammember
+    * @return string representation of a teammember
+    */
    public String toString()
    {
        //name + " " + startDate;
+      return name + " " + date.toString();
+
    }
 
    public static void main(String [] args)
