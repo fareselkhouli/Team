aa7ff1d74ff090b8da0e8908b56905be508ea483
Elaineanif <61065936+Elaineanif@users.noreply.github.com>
2020-02-17 02:02:31 -0500

    Add files via upload

--- src/Team.java
+++ src/Team.java
@@ -13,10 +13,21 @@ public class Team
    {
       //this is the default constructor
    }
-   
+   /**
+    * Search for the member to be removed
+    * 
+    */
    private int find(TeamMember m)
    {
-       
+	   int a = 0;
+       while(a != team.length) {
+    	   if (team[a].name == m.name && team[a].startDate == m.startDate) {
+    		   remove(team[a]);
+    		   break;
+    	   }
+    	   a++;
+       }
+       return a;
    }
 
    /**
@@ -35,10 +46,17 @@ public class Team
       }
 
    }
-   
+   /**
+    * If the list is empty, return true, else return false.
+    * @return
+    */
    public boolean isEmpty()
    {
-       
+       if(numMembers == 0) {
+    	   return true;
+       } else {
+    	   return false;
+       }
    }
 
    /**
@@ -55,18 +73,53 @@ public class Team
       numMembers++;
    }
    
+   /**
+    * 
+    * This method removes a teammember from the team. If the find method find the same name
+    * and the date it will remove the teammember.
+    * @param teammember to be removed
+    */
    public boolean remove(TeamMember m)
    {
-       
+	   int a=find(m);
+	   TeamMember [] newarray;
+       newarray = new TeamMember[numMembers-1];
+       int count=0,number=0;
+       while(count != numMembers) {
+    	   if(count == a) {
+    		   continue;
+    	   }
+    	   newarray[number] = team[count]; 
+    	   count++;
+    	   number++;
+       }
    } 
    
+   /**
+    * If the teammember is already in the team, return false, else return true.
+    * @param m
+    * @return
+    */
    public boolean contains(TeamMember m)
    {
-      
+      int x=0;
+      while(x != team.length) {
+   	   if (team[x].name == m.name && team[x].startDate == m.startDate) {
+   		   return false;
+   		   break;
+   	   }
+   	   x++;
+      }
+      return true;
    } 
    
    public void print()
    {
       //set up a for loop and call the toString() method
-   } 
-}
+	   int count = 0;
+		   while (count <= team.length) {
+			   System.out.print(team[count].name+toString());
+			   count++;
+		   }
+
+   }
