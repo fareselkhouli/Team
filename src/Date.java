c960c6a6be7addd5664194b9dcec3379e19effd9
fareselkhouli <fareselkhouli@gmail.com>
2020-02-17 14:09:33 -0500

    Update Date.java
    fixed methods

--- src/Date.java
+++ src/Date.java
@@ -18,9 +18,9 @@ public class Date
    {
       //use StringTokenizer to parse the String and create a Date object
        StringTokenizer date = new StringTokenizer(d,"/");
-       this.month = date.nextToken();
-       this.day = date.nextToken();
-       this.year = date.nextToken();
+       this.month = Integer.parseInt(date.nextToken());
+       this.day = Integer.parseInt(date.nextToken());
+       this.year = Integer.parseInt(date.nextToken());
    }
 
    /**
@@ -43,31 +43,33 @@ public class Date
    public boolean isValid()
    {
       //initialize constants
-      public static final int dayMax = 31;
-      public static final int dayMin = 1;
-      public static final int monthMax = 12;
-      public static final int monthMin = 1;
-      public static final int leapFeb = 29;
+      int dayMax = 31;
+      int dayMin = 1;
+      int monthMax = 12;
+      int monthMin = 1;
+      int leapFeb = 29;
+      int month = this.month;
+      int day = this.day;
 
-      if(day > dayMax || day < dayMin || month > monthMax || month < monthMin){
+      if((day > dayMax) || (day < dayMin) || (month > monthMax) || (month < monthMin)){
          return false;
       }
 
-      if(month == Month.JAN || month == Month.MAR || month == Month.MAY || month = Month.JUL
-              || month == Month.AUG || month = Month.OCT || month = Month.DEC){
-         return day == Month.DAYS_ODD;
+      if((month == Month.JAN) || (month == Month.MAR) || (month == Month.MAY) || (month == Month.JUL) ||
+               (month == Month.AUG) || (month == Month.OCT) || (month == Month.DEC)){
+         return (day == Month.DAYS_ODD);
       }
 
-      if(month == Month.APR || month == Month.JUN || month = Month.SEP || month = Month.NOV){
-         return day == Month.DAYS_EVEN;
+      if((month == Month.APR) || (month == Month.JUN) || (month == Month.SEP) || (month == Month.NOV)){
+         return (day == Month.DAYS_EVEN);
       }
 
-      if(month = 2){
+      if(month == 2){
          if(isLeapYear(year)){
-            return day == leapFeb;
+            return (day == leapFeb);
          }
          else{
-            return day == Month.DAYS_FEB;
+            return (day == Month.DAYS_FEB);
          }
       }
 
@@ -103,7 +105,7 @@ public class Date
    public String toString()
    {
        //use the format "month/day/year"
-      return month + "/" + day + "/" year;
+      return this.month + "/" + this.day + "/" + this.year;
    }
 
    /**
@@ -114,7 +116,8 @@ public class Date
    @Override
    public boolean equals(Object obj)
    {
-      if(obj.day = this.day && obj.month = this.month && obj.year == this.year){
+      Date currDate = (Date) obj;
+      if(currDate.day == this.day && currDate.month == this.month && currDate.year == this.year){
          return true;
       }
       else{
